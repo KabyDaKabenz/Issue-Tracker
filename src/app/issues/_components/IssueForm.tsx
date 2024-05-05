63dc454400a579bc329887652a32b8df9c60f3e9
KabyDaKabenz <karim.uug007@gmail.com>
2024-05-05 20:13:06 +0400

    Frontend: Added update issue functionality

--- src/app/issues/_components/IssueForm.tsx
+++ src/app/issues/_components/IssueForm.tsx
@@ -53,7 +53,11 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         onSubmit={handleSubmit(async (data) => {
           try {
             setIsSubmitting(true);
-            await axios.post("/api/issues", data);
+            if (issue) {
+              await axios.patch(`/api/issues/${issue.id}`, data);
+            } else {
+              await axios.post("/api/issues", data);
+            }
             router.push("/issues");
           } catch (error) {
             setIsSubmitting(false);
