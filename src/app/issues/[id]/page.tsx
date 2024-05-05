d7ff5d3051fd79a0f21f3f415d20bcda6c5a11d3
KabyDaKabenz <karim.uug007@gmail.com>
2024-05-05 11:51:34 +0400

    Added markdown preview

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import prisma from "../../../../prisma/client";
 import { Card, Heading, Text } from "@radix-ui/themes";
 import IssueStatusBadge from "@/components/IssueStatusBadge";
+import ReactMarkdown from "react-markdown";
 
 interface IssueDetailProps {
   params: {
@@ -25,8 +26,8 @@ const IssueDetailPage = async ({ params }: IssueDetailProps) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </div>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className="prose mt-4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   );
